06ea3cb2cb7cff60d6bc552d4c574c914abce2fd
Javier Almarza <javieralmarzabucarey@gmail.com>
2022-06-01 15:56:26 -0400

    manejo de la pagina 404

---
+++ src/containers/notFound/NotFoundContainer.jsx
@@ -0,0 +1,17 @@
+// Dependencias.
+import * as React from "react";
+
+// Importación de componentes.
+import NotFoundPage from "~pages/notFound/NotFoundPage.jsx";
+
+// Definición del contenedor.
+const NotFoundContainer = (props) => {
+  return (
+    <>
+      <NotFoundPage />
+    </>
+  );
+};
+
+// Exportación del contenedor.
+export default NotFoundContainer;
---
+++ src/pages/notFound/NotFoundPage.jsx
@@ -0,0 +1,19 @@
+// Dependencias.
+import React from "react";
+
+// Importación de estilos.
+import "./NotFoundPage.scss";
+
+// Definición de la pagina: Index.
+const NotFoundPage = () => {
+  return (
+    <section className="notFound_container">
+      <div className="title">
+        <h1>404 - Page</h1>
+      </div>
+    </section>
+  );
+};
+
+// Exportación de la pagina: Index.
+export default NotFoundPage;
