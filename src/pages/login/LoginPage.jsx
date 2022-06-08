67a747c7fa362b5f560d13fc6252eed63a215b6c
Javier Almarza <javieralmarzabucarey@gmail.com>
2022-06-08 11:35:37 -0400

    visual de login listo

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,16 +1,58 @@
 // Dependencias.
 import React from "react";
+import { TextField, Button } from "@mui/material";
+import { FaConciergeBell } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 // Importación de estilos.
 import "./LoginPage.scss";
 
 // Definición de la pagina: Index.
 const LoginPage = () => {
+  // Renderizado visual.
   return (
-    <section className="login_container">
-      <div className="title">
-        <h1>Login - Page</h1>
-      </div>
+    <section className="loginPage_container_page">
+      {/* Contenedor del Formulario.*/}
+      <section className="loginPage_container_form">
+        {/* Formulario de inicio de sesión */}
+        <form className="loginPage_form">
+          {/* Banner: titulo y texto */}
+          <div className="loginPage_form_banner">
+            <FaConciergeBell className="loginPage_form_banner_icon" />
+            <h1>PAYADMIN</h1>
+          </div>
+
+          {/* Input de texto */}
+          <div className="loginPage_form_textInput">
+            <p>Inicio de sesión</p>
+            <TextField
+              variant="filled"
+              label="Usuario"
+              type="text"
+              required={true}
+              className="loginPage_form_textInput-username"
+              id="input_username"
+            />
+
+            <TextField
+              variant="filled"
+              label="Contraseña"
+              type="password"
+              required={true}
+              className="loginPage_form_textInput-username"
+              id="input_username"
+            />
+            <p>¿Olvidaste tu contraseña?</p>
+          </div>
+
+          {/* Botón, ingresar */}
+          <div className="loginPage_form_btn">
+            <Link to="/caja">
+              <Button variant="contained">Ingresar</Button>
+            </Link>
+          </div>
+        </form>
+      </section>
     </section>
   );
 };
