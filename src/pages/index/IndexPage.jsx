da8ab74f9324553debbd893dff4f4b3c929b594d
Gonzalo cañas <gonzalo.benji@gmail.com>
2022-08-12 13:13:01 -0400

    mi primer commit

--- src/pages/index/IndexPage.jsx
+++ src/pages/index/IndexPage.jsx
@@ -29,7 +29,7 @@ class IndexPage extends Component {
     return (
       <section className="">
         <div className="">
-          <h1>Index - Page</h1>
+          <h1>PayAdmin</h1>
         </div>
       </section>
     );
