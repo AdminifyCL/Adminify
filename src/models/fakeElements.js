2f5b2913550fbeed9760acb5bcd475410b2d1aa7
Javier Almarza <javieralmarzabucarey@gmail.com>
2022-08-18 11:47:04 -0400

    vista para la lista de productos

---
+++ src/models/fakeElements.js
@@ -0,0 +1,63 @@
+// Definición de datos de prueba.
+const fakeElements = [
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+
+  {
+    icono: "tocomple",
+    id: "tocomple001",
+    nombre: "Completo italiano",
+    valor: "$1000",
+    disponibilidad: true,
+    unidades: 0,
+  },
+];
+
+// Exportación
+export default fakeElements;
--- src/pages/inventario/InventarioPage.jsx
+++ src/pages/inventario/InventarioPage.jsx
@@ -1,6 +1,8 @@
 // Dependencias.
 import React, { Component } from "react";
+import { Button } from "@mui/material";
 import PropTypes from "prop-types";
+import fakeElements from "../../models/fakeElements.js";
 import TabNavigation from "../../components/TabNavigation/TabNavigation.jsx";
 
 // Importación de estilos.
@@ -23,7 +25,63 @@ class InventarioPage extends Component {
   // -- Métodos.
   // -- Métodos [REDIRECT].
   // -- Métodos [HANDLER].
+  handleCreateProduct = () => {
+    console.log("[] Agregando producto...");
+  };
+
   // -- Métodos [MAPPING].
+  mappingElements = () => {
+    const elements = [...fakeElements];
+
+    // Recorriendo el arreglo de elementos.
+    return elements.map((element, index) => {
+      return (
+        <div className="inventarioPage_content_element" key={`product-${index}`}>
+          {/* Icono column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.icono}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Identificador column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.id}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Nombre column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.nombre}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Valor column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.valor}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Disponibilidad column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.disponibilidad ? "Sí" : "No"}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Unidades column */}
+          <div className="inventarioPage_content_column">
+            <p>{element.unidades}</p>
+            <div>contenedor</div>
+          </div>
+
+          {/* Botones column */}
+          <div className="inventarioPage_content_element-button">
+            <Button variant="contained">Editar</Button>
+            <Button variant="outlined">Eliminar</Button>
+          </div>
+        </div>
+      );
+    });
+  };
 
   // Renderizado.
   render() {
@@ -36,7 +94,18 @@ class InventarioPage extends Component {
 
         {/* Vista de la caja. */}
         <section className="inventarioPage_content">
-          <h1>Inventario</h1>
+          {/* Titulo y boton de acción */}
+          <div className="inventarioPage_content_header">
+            <h1>Administración de inventario</h1>
+            <Button variant="contained" onClick={() => this.handleCreateProduct()}>
+              Agregar producto
+            </Button>
+          </div>
+          {/* Contenedor de elementos. */}
+          <div className="inventarioPage_content_container">
+            {/* Elementos de la tabla. */}
+            <div className="inventarioPage_content_container-element">{this.mappingElements()}</div>
+          </div>
         </section>
       </section>
     );
