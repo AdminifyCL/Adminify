e65d51b1f89f4f303b111dc689f4f601aabb65a4
Nicko <Nickocruz4513@gmail.com>
2022-09-13 14:41:43 -0300

    Cambie las posiciones y arregle el buscador

--- src/pages/caja/CajaPage.jsx
+++ src/pages/caja/CajaPage.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import TabNavigation from "../../components/TabNavigation/TabNavigation.jsx";
 import fakeElements from "../../models/fakeElements.js";
-import {VistaTotal} from "./VistaTotal";
 import { Button } from "@mui/material";
 import { FaHamburger, FaHotdog } from "react-icons/fa";
 import { getDatabase } from "firebase/database"
+import { Menu } from "./Menu"
 
 // Importación de estilos.
 import "./CajaPage.scss";
@@ -16,7 +16,13 @@ const productos = [...fakeElements];
 const data = getDatabase()
 
 function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1);
+  if (word.length>1){
+    return word[0].toUpperCase() + word.slice(1);
+  }else if (word.length == 1){
+    return word.toUpperCase()
+  }else{
+    return word
+  }
 }
 
 // Definición de la pagina: Index.
@@ -24,7 +30,7 @@ class CajaPage extends Component {
   // -- Constructor.
   constructor(props) {
     super(props);
-    this.state = { total: 0 , carro : [], finder :""};
+    this.state = { total: 0 , carro : [], finder :"", pago: true};
   }
 
   // -- Ciclo de vida del componente.
@@ -39,7 +45,7 @@ class CajaPage extends Component {
 
   // Renderizado.
   render() {
-    return (
+    return (( this.state.pago ? 
       <section className="cajaPage_container">
         {/* Navegación de la aplicación. */}
         <section className="cajaPage_navigation">
@@ -48,9 +54,10 @@ class CajaPage extends Component {
         <h1>
           {/* Vista de la caja. */}
           <section className="cajaPage_content">
-            <h1 className="cajaPage_titulo"> Sistema de caja </h1>
             {/* Division en la que se ven los productos */}
             <div className="cajaPage_Productos">
+              <div className="cajaPage_Productos_cabecera">
+              <p>Lista de productos</p>
               <input
                 type={"search"}
                 className="cajaPage_Buscador"
@@ -58,12 +65,13 @@ class CajaPage extends Component {
                   this.setState({ finder: e.target.value });
                 }}
               ></input>
+              </div>
+              <div className="cajaPage_Productos_lista">
               {productos.map((producto) => {
-                if (producto.nombre.startsWith(this.state.finder.toUpperCase())) {
+                if (producto.nombre.startsWith(capitalize(this.state.finder))) {
                 return (
                   <div className="cajaPage_Productos_producto">
-                    <FaHamburger></FaHamburger>
-                    {/*}<p className="cajaPage_producto_texto">{producto.icono}</p>{*/}
+                    <p><FaHamburger></FaHamburger></p>
                     <p className="cajaPage_producto_texto" style={{ width: "40%" }}>
                       {producto.nombre}
                     </p>
@@ -92,6 +100,19 @@ class CajaPage extends Component {
                               },
                             ],
                           })
+                        }else{
+                          this.setState({
+                            carro: this.state.carro.map((item) => {
+                              if (producto.cantidad > 1) {
+                                this.setState({ total: this.state.total + producto.precio });
+                              }
+                              if (item.nombre == producto.nombre) {
+                                item.cantidad = item.cantidad + 1
+                              }
+                              return item;
+                            }),
+                          });
+                          this.setState({ total: this.state.total + producto.valor });
                         }
                       }}
                     >
@@ -100,11 +121,17 @@ class CajaPage extends Component {
                   </div>
                 );}
               })}
+              </div>
             </div>
-
+            {/*Division en la que se ven los datos del cajero*/}
+            <div className="cajaPage_Cajero">
+              <div className="cajaPage_Cajero_cabecera">Bienvenido usuario</div>
+            </div>
             {/* Division en la que se ve el carro */}
             <div className="cajaPage_Carro">
-              {/*}<h1 className="cajaPage_Carro_titulo"> Carro de compras </h1>{*/}
+              <div className="cajaPage_Carro_cabecera">
+                <p> Carrito </p>
+              </div>
               {this.state.carro.map((elemento) => {
                 return (
                   <div className="cajaPage_Carro_producto">
@@ -112,47 +139,61 @@ class CajaPage extends Component {
                       {elemento.nombre}
                     </p>
                     <div className="cajaPage_carro_cantidad">
-                      
-                      <button className="cajaPage_carro_boton" onClick={()=>{
-                        this.setState({ carro : this.state.carro.map((item)=>{
-                          if (item.nombre == elemento.nombre){
-                            item.cantidad = item.cantidad+1}
-                          return item}) });
-                        this.setState({total: this.state.total+elemento.precio})
-                      }}>+</button>
+                      <button style={{"padding-bottom":"4px"}}
+                        className="cajaPage_carro_boton"
+                        onClick={() => {
+                          this.setState({
+                            carro: this.state.carro.map((item) => {
+                              if (elemento.cantidad > 1) {
+                                this.setState({ total: this.state.total - elemento.precio });
+                              }
+                              if (item.nombre == elemento.nombre) {
+                                item.cantidad = item.cantidad - 1 > 0 ? item.cantidad - 1 : 1;
+                              }
+                              return item;
+                            }),
+                          });
+                        }}
+                      >
+                        -
+                      </button>
 
-                      <p> {(elemento.cantidad>=1)? elemento.cantidad: 1} </p>
+                      <p style={{margin: "1px 5px 1px 5px",width:"20%"}}> {elemento.cantidad >= 1 ? elemento.cantidad : 1} </p>
 
-                      <button className="cajaPage_carro_boton"onClick={()=>{
-                        this.setState({ carro : this.state.carro.map((item)=>{
-                          if (elemento.cantidad>1){this.setState({ total: this.state.total - elemento.precio });}
-                          if ((item.nombre == elemento.nombre)){
-                            item.cantidad = (item.cantidad-1>0)? item.cantidad-1 : 1}
-                          return item}) });
-                      }}>-</button>
+                      <button
+                        className="cajaPage_carro_boton"
+                        onClick={() => {
+                          this.setState({
+                            carro: this.state.carro.map((item) => {
+                              if (item.nombre == elemento.nombre) {
+                                item.cantidad = item.cantidad + 1;
+                              }
+                              return item;
+                            }),
+                          });
+                          this.setState({ total: this.state.total + elemento.precio });
+                        }}
+                      >
+                        +
+                      </button>
                     </div>
-                    <p className="cajaPage_producto_texto">${(elemento.cantidad>=1)? elemento.cantidad*elemento.precio: elemento.precio}</p>
+                    <p className="cajaPage_producto_texto">
+                      $
+                      {elemento.cantidad >= 1
+                        ? elemento.cantidad * elemento.precio
+                        : elemento.precio}
+                    </p>
                   </div>
                 );
               })}
             </div>
             {/* Division en la que se ve el total */}
             {/* <VistaTotal total={this.state.total} /> */}
-            <div className="cajaPage_Total">Total : ${this.state.total}</div>
+            <div className="cajaPage_Total"><p>Total </p><p className="cajaPage_Total_valor">${this.state.total}</p></div>
             <div className="cajaPage_Botones">
-              <Button
-                variant="contained"
-                style={{ height: "60px", width: "130px" }}
-                onClick={() => {
-                  alert("Esta entrando al menu de pago...");
-                  console.log(this.state.carro)
-                }}
-              >
-                Pagar
-              </Button>
-              <Button
+            <Button
                 variant="outlined"
-                style={{ height: "60px", width: "130px" }}
+                style={{ height: "50px", width: "130px" }}
                 onClick={() => {
                   this.setState({
                     carro: [],
@@ -164,11 +205,22 @@ class CajaPage extends Component {
               >
                 Limpiar
               </Button>
+              <Button
+                variant="contained"
+                style={{ height: "50px", width: "130px" }}
+                onClick={() => {
+                  console.log("Pagando...")
+                  this.setState({pago: (!this.state.pago)})
+                }}
+              >
+                Pagar
+              </Button>
+              
             </div>
           </section>
         </h1>
       </section>
-    );
+    : <Menu></Menu>));
   }
 }
 
