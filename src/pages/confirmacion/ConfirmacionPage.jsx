b3b078040c85b4048a41bf32e5478318f897d053
Javier Almarza <javieralmarzabucarey@gmail.com>
2022-09-16 11:35:53 -0400

    probando la impresora

--- src/pages/confirmacion/ConfirmacionPage.jsx
+++ src/pages/confirmacion/ConfirmacionPage.jsx
@@ -1,5 +1,6 @@
 // Dependencias.
 import React from "react";
+import { Button } from "@mui/material";
 import TabNavigation from "../../components/TabNavigation/TabNavigation.jsx";
 
 // Estilos.
@@ -10,17 +11,31 @@ const ConfirmacionPage = ({}) => {
   // -- Manejo del estado.
   // -- Ciclo de vida.
   // -- Metodos.
+  const handleImpresion = () => {
+    // const ventana = window.open("", "PRINT", "height=720,width=1280");
+    // ventana.document.write("Hola, esto es una prueba jeje");
+    // ventana.document.close();
+    // ventana.focus();
+    // ventana.onload = function () {
+    //   ventana.print();
+    //   ventana.close();
+    // };
+  };
+
   // -- Renderizado.
   return (
-    <section>
+    <section className="">
       {/* Navegación */}
       <section className="">
         <TabNavigation />
       </section>
 
       {/* Visual */}
-      <section className="">
+      <section className="ConfirmaciónPage-container">
         <h1>Vista: confirmación del pago</h1>
+        <Button onClick={() => handleImpresion()} variant="contained">
+          Imprimir
+        </Button>
       </section>
     </section>
   );
