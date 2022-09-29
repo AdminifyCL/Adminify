0359a296766ac402d7ff0c436ff8f671a9634d07
Javier Almarza <javieralmarzabucarey@gmail.com>
2022-09-29 11:18:35 -0400

    metodo de pago

--- src/actions/ventas/setMetodoPago.js
+++ src/actions/ventas/setMetodoPago.js
@@ -1,27 +1,27 @@
-// // Dependencias.
-// // ...
+// Dependencias.
+// ...
 
-// // Configuraciones.
-// // import { actionProductTypes } from "../../types/action.js";
-// import { onSuccess, onError } from "../response.js";
-// // const { deleteProduct: TYPE } = actionProductTypes;
+// Configuraciones.
+// import { actionProductTypes } from "../../types/action.js";
+import { onSuccess, onError } from "../response.js";
+// const { deleteProduct: TYPE } = actionProductTypes;
 
-// const setMetodoPago = () => {
-//   // console.log(`[🛂][ACTION][${TYPE}]`);
+const setMetodoPago = () => {
+  // console.log(`[🛂][ACTION][${TYPE}]`);
 
-//   // Fetch
-//   return async (dispatch) => {
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         onSuccess(dispatch, TYPE, {});
-//         resolve();
-//       } catch (error) {
-//         onError(dispatch, TYPE, error);
-//         reject(error);
-//       }
-//     });
-//   };
-// };
+  // Fetch
+  return async (dispatch) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        onSuccess(dispatch, TYPE, {});
+        resolve();
+      } catch (error) {
+        onError(dispatch, TYPE, error);
+        reject(error);
+      }
+    });
+  };
+};
 
-// // Exportación.
+// Exportación.
 // export { setMetodoPago };
