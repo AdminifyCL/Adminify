52c277055b40ce739b99e0e3101f1d7bb4059351
Nicko <Nickocruz4513@gmail.com>
2022-10-18 21:50:25 -0300

    Cambios en el cierre

--- src/pages/caja/components/CajaCierre.jsx
+++ src/pages/caja/components/CajaCierre.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "../CajaPage.scss";
 import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+
+import userSignOut from "../../../api/usuarios/userLogout.js";
+import { userLogout } from "../../../redux/slices/usuariosSlice.js";
 
 // Definición del componente: <CajaBotones />
 export function CajaCierre(props) {
@@ -8,27 +12,41 @@ export function CajaCierre(props) {
 
   const visibilidad = props.open;
 
+  const dispatch = useDispatch()
+
+  const handleSignOut = async () => {
+    await userSignOut()
+      .then(() => {
+        dispatch(userLogout({}));
+      })
+      .catch((error) => {
+        console.error("[] ERROR:", error);
+      });
+  };
+
   if (visibilidad) {
     return (
       <div className="cajaPage_menu_cierre">
         ¿ Esta seguro de realizar el cierre de caja ?
         <div>
-          <Button
+        <Button
             variant="contained"
-            onclick={() => {
+            onClick={() => {
               //CODIGO DE CIERRE DE CAJA
+              handleSignOut()
             }}
           >
             Aceptar
           </Button>
-          <Button
-            variant="contained"
+        <Button
+            variant="outlined"
             onClick={() => {
               props.cerrar();
             }}
           >
             Salir
           </Button>
+          
         </div>
       </div>
     );
