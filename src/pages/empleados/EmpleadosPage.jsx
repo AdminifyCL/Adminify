51f8e54c07cb180348ab658a82aeea608891f4c7
Nicko <Nickocruz4513@gmail.com>
2022-10-24 23:59:10 -0300

    Eliminando la mejor vista de la historia

--- src/pages/empleados/EmpleadosPage.jsx
+++ src/pages/empleados/EmpleadosPage.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import Navigation from "../../components/Navigation/Navigation.jsx";
 import PropTypes from "prop-types";
 import { publicURL, privateURL } from "../../schemas/Navigation.js";
-import EmployeeModal from "../../components/Modals/Employees/EmployeeModal.jsx";
 import {BsPersonFill} from "react-icons/bs";
 
 // Estilos.
@@ -14,12 +13,6 @@ import { Button } from "@mui/material";
 const EmpleadosPage = (props) => {
   // 1. Manejo del estado.
   const {} = props;
-  const empleados = [{"nombre":"Javier Almarza","correo":"javier@gmail.com","cargo":"Empleado"}] 
-  const [modalVisibility,setModalVisibility] = useState(false)
-
-  const handleVisibility = () => {
-    setModalVisibility(!modalVisibility);
-  };
 
   // 2. Ciclo de vida.
   useEffect(() => {}, []);
@@ -31,32 +24,6 @@ const EmpleadosPage = (props) => {
       {/* Navegación de la aplicación. */}
       <section className="empleadosPage_navigation">
         <Navigation />
-      </section>
-
-      <EmployeeModal
-        open={modalVisibility}
-        onClose={() => handleVisibility()}
-      />
-
-      {/* Vista del empleado. */}
-      <section className="empleadosPage_content">
-        <div className="empleadosPage_Cabecera">
-        <p>Administracion de empleados</p><Button variant="contained" onClick={handleVisibility}>Agregar empleados</Button>
-        </div>
-        <div className = "empleadosPage_Contenido">
-        {empleados.map((employee)=>{
-          return <div className="empleadosPage_Tarjeta"> 
-          <div className="empleadosPage_icono">
-          <BsPersonFill size={50}></BsPersonFill>
-          </div>
-            <div>
-            <h2 >{employee.nombre}</h2>
-            <h5 >{employee.correo}</h5>
-            <p >{employee.cargo}</p>
-            </div>
-          </div>
-        })}
-        </div>
       </section>
     </section>
   );
